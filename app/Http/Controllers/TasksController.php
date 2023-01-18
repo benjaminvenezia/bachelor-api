d3a9d1f409919cb11c1660a7868bdaefd4ef4510
Benjamin <benjaminvenezia@hotmail.com>
2023-01-18 15:46:06 +0100

    [func] add task resource.

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TasksController extends Controller
@@ -13,6 +14,8 @@ class TasksController extends Controller
      */
     public function index()
     {
+
+        return Task::all();
     }
 
     /**
