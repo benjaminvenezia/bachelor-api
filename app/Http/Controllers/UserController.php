bdf3722cf2e59dcc54227e883d57ff990b7e86ef
Benjamin <Benjaminvenezia@hotmail.com>
2023-02-26 11:34:43 +0100

    update the update method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -20,5 +21,48 @@ class UserController extends Controller
         );
 
         return $users;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        if (Auth::user()->id !== $user->id) {
+            // return $this->error('', 'You are not authorized to make this request', 403);
+            $errorHandling = [
+                'message' => "You are not authorized to make this request",
+                'code' => 403
+            ];
+
+            return json_encode($errorHandling);
+        }
+
+        $user->update($request->all());
+
+        return new UserResource($user);
+    }
+
+    // /**
+    //  * Display the specified resource.
+    //  *
+    //  * @param  int  $id
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function show(User $user)
+    // {
+    //     return $this->isNotAuthorized($user) ? $this->isNotAuthorized($user) : new UserResource($user);
+    //     //$task = Task::find($id); pas nécessaire cf shortcut. :)
+    // }
+
+    private function isNotAuthorized($user)
+    {
+        if (Auth::user()->id !== $user->id) {
+            return $this->error('', 'You are not authorized to make this request', 403);
+        }
     }
 }
