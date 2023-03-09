664073e66148290dfb3ba0a4299a93526709acb5
Benjamin <Benjaminvenezia@hotmail.com>
2023-03-09 18:14:12 +0100

    Add a gages migration

--- app/Models/Gage.php
+++ app/Models/Gage.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Gage extends Model
 {
     use HasFactory;
+
+
+    //     title: string;
+    //   description: string;
+    //   category: string;
+    //   isDone: boolean;
+    //   dateString: string;
+    //   day: number;
+    //   month: number;
+    //   year: number;
+    //   timestamp: number;
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'id',
+        'name',
+        'user_id1',
+        'user_id2',
+    ];
 }
