f62aa2f7ba90cb9637c001507823a3e3660d9bc3
Benjamin <Benjaminvenezia@hotmail.com>
2023-03-09 18:41:15 +0100

    update the UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,10 +18,13 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'personalCode' => $this->faker->unique()->regexify('[A-Z]{3}[0-9]{3}'),
+            'otherCode' => $this->faker->regexify('[A-Z]{4}[0-9]{4}'),
+            'name' => $this->faker->name,
+            'points' => $this->faker->numberBetween(0, 1000),
+            'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('password'), // mot de passe pour tous les utilisateurs générés par la factory
             'remember_token' => Str::random(10),
         ];
     }
