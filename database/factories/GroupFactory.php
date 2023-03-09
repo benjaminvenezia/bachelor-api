b49b5982a53f7ede19aff86c8cbb2071083ffc78
Benjamin <Benjaminvenezia@hotmail.com>
2023-03-09 18:52:22 +0100

    continue to update the catories

---
+++ database/factories/GroupFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Group>
+ */
+class GroupFactory extends Factory
+{
+    protected $model = Group::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word,
+            'user_id1' => User::factory(),
+            'user_id2' => User::factory(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Gage;
+use App\Models\Group;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -18,46 +22,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-        $usersToAdd = [
-            [
-                'id' => 1,
-                'name' => "Thierry",
-                'personalCode' => 'aaa',
-                'otherCode' => 'bbb',
-                'email' => "ben@gmail.com",
-                'password' => Hash::make("password"),
-                'points' => 0,
-            ],
-            [
-                'id' => 2,
-                'name' => "Marjorie",
-                'personalCode' => 'bbb',
-                'otherCode' => 'aaa',
-                'email' => "noemi@gmail.com",
-                'password' => Hash::make("password"),
-                'points' => 0,
-            ],
-            [
-                'id' => 3,
-                'name' => "Armand",
-                'personalCode' => 'ccc',
-                'otherCode' => 'ddd',
-                'email' => "armand@gmail.com",
-                'password' => Hash::make("password"),
-                'points' => 0,
-            ],
-            [
-                'id' => 4,
-                'name' => "Dedet",
-                'personalCode' => 'ddd',
-                'otherCode' => 'ccc',
-                'email' => "dedet@gmail.com",
-                'password' => Hash::make("password"),
-                'points' => 0,
-            ]
-        ];
 
-        DB::table('users')->insert($usersToAdd);
+        // création de deux utilisateurs avec la factory
+        $user1 = User::factory()->create();
+        $user2 = User::factory()->create();
+
+        // création de 5 gages pour chaque utilisateur
+        Gage::factory()->count(5)->create(['user_id' => $user1->id]);
+        Gage::factory()->count(5)->create(['user_id' => $user2->id]);
+
+        Group::factory(Group::class, 3)->create();
+
 
 
         DB::table('groups')->insert(
