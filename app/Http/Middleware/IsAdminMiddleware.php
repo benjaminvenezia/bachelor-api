27d2e3a4febd4e8a54afca2b731daa8446b33131
Benjamin <benjaminvenezia@hotmail.com>
2023-04-05 17:14:18 +0200

    refactor the IsAdminMiddleware

--- app/Http/Middleware/IsAdminMiddleware.php
+++ app/Http/Middleware/IsAdminMiddleware.php
@@ -3,25 +3,37 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Traits\HandlesDatabaseErrors;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
 
 class IsAdminMiddleware
 {
+    use HandlesDatabaseErrors;
+
     /**
      * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
-    
-        if (!auth()->check() || !auth()->user()->is_admin) {
-            abort(403, "Vous tentez d'accéder à une action d'administration.");
-        }
+        try {
+            $user = Auth::user();
 
-        return $next($request);
+            if (!$user) {
+                throw new Exception('L\'utilisateur n\'est pas authentifié', 401);
+            }
+    
+            if (!$user->is_admin) {
+                abort(403, "Vous tentez d'accéder à une action d'administration.");
+            }
+    
+            return $next($request);
+
+        } catch(Exception $e) {
+            return HandlesDatabaseErrors::handleDatabaseError($e);
+        }
     }
 }
